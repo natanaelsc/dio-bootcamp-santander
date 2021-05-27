50c1906dfd37e672dc797fc67962336b7abf2e19
Natanael Silva <natanael96@live.com>
2021-05-26 21:57:54 -0300

    First Commit

---
+++ src/main/java/com/project/bootcampsantander/BootcampSantanderApplication.java
@@ -0,0 +1,25 @@
+package com.project.bootcampsantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@SpringBootApplication
+public class BootcampSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantanderApplication.class, args);
+	}
+
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+		.title("")
+		.version("1.0")
+		.termsOfService("http://swagger.io/terms")
+		.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+
+}
