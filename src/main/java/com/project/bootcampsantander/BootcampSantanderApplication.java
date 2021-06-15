faa9a82009d73400619edcdc17b00f077e3d5a4a
Natanael Silva <natanael96@live.com>
2021-06-15 10:57:18 -0300

    OpenAPI

--- src/main/java/com/project/bootcampsantander/BootcampSantanderApplication.java
+++ src/main/java/com/project/bootcampsantander/BootcampSantanderApplication.java
@@ -1,7 +1,9 @@
 package com.project.bootcampsantander;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -15,9 +17,10 @@ public class BootcampSantanderApplication {
 	}
 
 	// Customização OpenAPI
-	public OpenAPI customOpenAPI() {
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description) {
 		return new OpenAPI().info(new Info()
-		.title("")
+		.title(description)
 		.version("1.0")
 		.termsOfService("http://swagger.io/terms")
 		.license(new License().name("Apache 2.0").url("http://springdoc.org")));
